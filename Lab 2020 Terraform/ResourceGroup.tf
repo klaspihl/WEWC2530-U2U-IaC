da17fef242f32bb227a2bd46b63b7a9db13ead55
Klas Pihl <klas.pihl@atea.se>
2022-11-01 10:16:06 +0100

    remove appending tags

--- Lab 2020 Terraform/ResourceGroup.tf
+++ Lab 2020 Terraform/ResourceGroup.tf
@@ -1,17 +1,17 @@
 resource "azurerm_resource_group" "ResourceGroup" {
   name     = "RG-${var.system}"
   location = var.location
-
-  tags = merge(
-      var.default_tags,
-      tomap(
-        "StartDate", formatdate("YYYY-MM-DD hh:mm", timestamp())
-        )
-      )
-    lifecycle {
-      ignore_changes = [
-        tags["StartDate"]
-      ]
-    }
+tags = var.default_tags
+  #tags = merge(
+  #    var.default_tags,
+  #    tomap(
+  #      "StartDate", formatdate("YYYY-MM-DD hh:mm", timestamp())
+  #      )
+  #    )
+  #  lifecycle {
+  #    ignore_changes = [
+  #      tags["StartDate"]
+  #    ]
+  #  }
 
 }
