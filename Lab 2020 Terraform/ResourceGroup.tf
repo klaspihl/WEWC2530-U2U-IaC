e65e7a930588823981bcd02bdc61547b5b1a62e9
Klas Pihl <klas.pihl@atea.se>
2022-11-01 10:24:53 +0100

    use of tomap instead of fixed variable

--- Lab 2020 Terraform/ResourceGroup.tf
+++ Lab 2020 Terraform/ResourceGroup.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "ResourceGroup" {
   name     = "RG-${var.system}"
   location = var.location
-tags = var.default_tags
+#tags = var.default_tags
   tags = merge(
       var.default_tags,
       tomap({
